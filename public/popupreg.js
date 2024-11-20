1d741ab265f71e9f6debbebea0203d426db1b17a
yokas <yokasdenis@gmail.com>
2024-11-20 09:00:09 +0300

    Update popupreg.js

--- public/popupreg.js
+++ public/popupreg.js
@@ -1,17 +1,17 @@
-const pop=document.getElementById("popup")
-window.onload =generatepopUp();
-function generatepopUp(data) {
-    axios
-      .post("/register", data)
-      .then((res) => {
-        if (res.data.status === 200) {
-          alert("Registration successful! Please check your email for verification.");
-          // Optionally redirect or perform other actions here
-          pop.classList.add("open-popup")
-        }
-      })
-      .catch((error) => {
-        console.error("There was an error with the registration request:", error);
-        alert("Registration failed. Please try again.");
-      });
-}
+// const pop=document.getElementById("popup")
+// window.onload =generatepopUp();
+// function generatepopUp(data) {
+//     axios
+//       .post("/register", data)
+//       .then((res) => {
+//         if (res.data.status === 200) {
+//           alert("Registration successful! Please check your email for verification.");
+//           // Optionally redirect or perform other actions here
+//           pop.classList.add("open-popup")
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("There was an error with the registration request:", error);
+//         alert("Registration failed. Please try again.");
+//       });
+// }
